5f2ba00cb043faa8a14913cc8f7d726d0746be35
sriyachilla <73271452+sriyachilla@users.noreply.github.com>
2020-11-18 11:08:16 -0800

    nav bar on subpages

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #connects default URL of server to a python function
-@app.route('/')
+@app.route('/home')
 def home():
     #function use Flask import (Jinja) to render an HTML template
     return render_template("home.html")
