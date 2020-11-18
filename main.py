5f54ee29ca636b8e979cf9d866214dae2e078392
sriyachilla <73271452+sriyachilla@users.noreply.github.com>
2020-11-18 14:13:25 -0800

    nav bar fixed & put nav on testknowledgegame.html subpage

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 #connects default URL of server to a python function
 @app.route('/')
-@app.route('/')
+@app.route('/home')
 def home():
     #function use Flask import (Jinja) to render an HTML template
     return render_template("home.html")
